886488c4a2984ce3c89175e3fa11ce7b22361fcc
Matthew Meadows <matthewmeadows1999@gmail.com>
2023-09-11 03:32:32 -0400

    Input Styling

--- src/AmountInput.tsx
+++ src/AmountInput.tsx
@@ -2,12 +2,13 @@ import Input, { InputProps } from "./Input";
 
 export default function AmountInput(props:InputProps){
     return(
-<div>
+<div className="flex items-center bg-blue-950 border border-white/10 rounded-lg ">
 <Input 
 placeholder = "Amount"
-   value={props.value} 
-   onChange={props.onChange} />
-  <span className= "text-white/50 px-4"> USD</span>
+className= "border-0 w-24 pl-4 bg-transparent text-2xl"
+value={props.value} 
+onChange={props.onChange} />
+<span className= "text-white/50 px-4"> USD</span>
 </div>
 
     );
