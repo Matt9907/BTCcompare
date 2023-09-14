3917667584d1a602e432804c421084ddfe0e3329
Matthew Meadows <matthewmeadows1999@gmail.com>
2023-09-14 01:53:02 -0400

    Only display results when input is provided

--- src/ResultRow.tsx
+++ src/ResultRow.tsx
@@ -1,17 +1,27 @@
 type ResultRowProps ={
     loading?: boolean;
+    providerName?: string;
+    btc?:string;
 }
 
-export default function ResultRow({loading}:ResultRowProps){
+export default function ResultRow({loading,providerName,btc}:ResultRowProps){
     return(
-        <div className="relative border min-h-12 border-white/10 rounded-lg bg-gradient-to-r from-purple-500/10 to-blue-500/10 p-4 my-2 overflow-hidden "> 
+        <div className="relative border min-h-[64px] border-white/10 rounded-lg bg-gradient-to-r from-purple-500/10 to-blue-500/10 p-4 my-2 overflow-hidden "> 
         <div className="flex gap-4">
-            <div>Logo</div>
-            <div className="grow">Provider Name</div>
+            {providerName && (
+                <div>Logo</div>
+
+            )}
+           
+            <div className="grow">{providerName || ''}</div>
+            {btc && (
+
+           
             <div className="flex gap-2">
             <span className="text-xl text-purple-200/80">0.000</span>
             <span className="text-xl text-purple-300/50">BTC</span>
             </div>
+             )}
         </div>
         {loading &&(
             <div className= "inset-0 absolute bg-gradient-to-r from-transparent via-blue-900/50 to transparent skeleton-animation" />
